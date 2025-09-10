8da835e55a124912c2c276cfcb6b958e5554e6ed
root <root@DESKTOP-BUGKGO7.>
2025-09-10 07:13:43 +0300

    after routing

--- server.js
+++ server.js
@@ -1,27 +1,71 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
-    //res.sendFile('./views/index.html', {root: __dirname});
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+//custom middleware logger
+app.use(logger);
 
-app.get(['/index', '/index.html'], (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+//cross origin resourse sharing middleware
+const whitelist = [
+    'https://www.raiapp.com', 
+    'http://127.0.0.1:5500', 
+    'http://localhost:3500'
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200 //for legacy browsers support
+};
+app.use(cors(corsOptions));
 
-app.get('/new-page.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
+//built-in middliware for form data
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/old-page.html', (req, res) => {
-    res.redirect(301, '/new-page.html');
-});
+//built-in middleware for json
+app.use(express.json());
 
-app.get('/*', (req, res) => {
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
+
+//routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('routes/api/employees'));
+
+//i tied this but the client side preferred the json to the user friendly 404 page
+// app.use((req, res) => {
+//     res.status(404);
+//     if (req.accepts('html')) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'));
+//     }
+//     if (req.accepts('json')) {
+//         res.json({ error: "404 Not Found" });
+//     } else {
+//         res.type('txt').send("404 Not Found");
+//     }
+// });
+
+app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//well, i tried but it didn't work
+// app.all('*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
